612d857501748818f83519a780e4265f70aea154
Michael Beck <stressfullycarefree@gmail.com>
2019-01-03 15:12:23 -0500

    added name, whitespace, and quotations practice

---
+++ quotations_practice.py
@@ -0,0 +1,8 @@
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+message = '\nOne of Python\'s strengths is its diverse community.'
+print(message)
+
+message = "\nNancy said \"I can't believe it's Tuesday!\""
+print(message)
---
+++ whitespace.py
@@ -0,0 +1,13 @@
+print("Python")
+print("\tPython")
+
+print("Languages:\nPython\nC\nJavascript")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = ' python '
+
+print(favorite_language)
+print(favorite_language.rstrip().lstrip())
+print(favorite_language.strip())
+
