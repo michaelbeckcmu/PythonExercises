3f1790649309842bc88618ed586fd87bcc146e11
Michael Beck <stressfullycarefree@gmail.com>
2019-04-12 17:16:47 -0700

    Started pygame and added speedup feature to ship

---
+++ Alien_Game/alien_invasion.py
@@ -0,0 +1,36 @@
+import pygame
+
+from settings import Settings
+
+from ship import Ship
+
+import game_functions as gf
+
+def run_game():
+		# Initialize pygame, settings, and screen object.
+		pygame.init()
+		ai_settings = Settings()
+		screen =  pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+		pygame.display.set_caption("Alien Invasion")
+
+		# Make a ship.
+		ship = Ship(ai_settings, screen)
+
+
+		# Set the background color.
+		bg_color = (230, 230, 230)
+
+
+		# Start the main loop for the game.
+		while True:
+
+				# Watch for keyboard and mouse events.
+				gf.check_events(ship)
+
+				# Redraw the screen during each pass through the loop.
+				gf.update_screen(ai_settings, screen, ship)
+
+				# Make the most recently drawn screen visible.
+				pygame.display.flip()
+
+run_game()
---
+++ Alien_Game/ship.py
@@ -0,0 +1,70 @@
+import pygame
+
+class Ship():
+
+	def __init__(self, ai_settings, screen):
+		"""Initialize the ship and set its starting position."""
+		self.screen = screen
+
+		self.settings = ai_settings
+
+		# Load the ship image and get its rect.
+		self.image = pygame.image.load('images/ship.bmp')
+		self.rect = self.image.get_rect()
+		self.screen_rect = screen.get_rect()
+
+		# Start each new ship at the bottom center of the screen.
+		self.rect.centerx = self.screen_rect.centerx
+		self.rect.bottom = self.screen_rect.bottom
+
+		# Store a decimal value for the ship's center.
+		self.center = float(self.rect.centerx)
+
+		self.moving_right = False
+		self.moving_left = False
+
+		self.speedup_counter = 0
+		self.speedup_flag = False
+
+		self.error_counter = 0
+
+
+	def update(self):
+
+
+		if self.moving_right and self.rect.right < self.screen_rect.right and self.multi_button_check():
+			self.center += (1+self.settings.ship_speedup_factor*self.speedup_counter)*\
+			(self.settings.ship_speed_factor)
+		elif self.moving_left and self.rect.left > 0 and self.multi_button_check():
+			self.center -= (1+self.settings.ship_speedup_factor*self.speedup_counter)*\
+			(self.settings.ship_speed_factor)
+
+		self.rect.centerx = self.center
+
+		if self.speedup_flag == False:
+			print(self.error_counter)
+			self.error_counter += 1
+
+		if self.speedup_flag:
+			self.speedup_counter += 1
+		
+		if (self.moving_right or self.moving_left) and self.multi_button_check():
+			self.speedup_flag = True
+		else:
+			self.speedup_flag = False
+			self.speedup_counter = 0
+
+
+	def multi_button_check(self):
+		if self.moving_left and self.moving_right:
+			return False
+		else:
+			return True
+
+
+	def blitme(self):
+		"""Check ship position and draw the ship at its current location."""
+		
+		self.update()
+
+		self.screen.blit(self.image, self.rect)
